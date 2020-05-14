097a3649ea173a71c362140a713743827516cee8
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-05-14 14:57:19 +0200

    feat(add-version) add test-run option

--- assets/add_version.sh
+++ assets/add_version.sh
@@ -2,6 +2,8 @@
 
 CODE_BASE=$1
 ADD_VERSION=$2
+DRY_RUN=$3
+
 LOCAL_PATH=$(dirname "$(realpath "$0")")/..
 
 # load variables
@@ -12,46 +14,56 @@ function usage {
 cat << EOF
 
 Usage:
-  add_version.sh [code-base] [version]
+  add_version.sh [code-base] [version] [test]
 
   code-base: required. Either "EE" for Kong Enterprise
              or "CE" for Kong open source.
   version:   required. The version of the product to add
              to Pongo.
+  test:      add "test" to make it a test run without pushing updates
 
 This tool will attempt to update Pongo by adding the requested version.
 EOF
 }
 
 
+if [[ ! "$DRY_RUN" == "test" ]]; then
+  if [[ ! "$DRY_RUN" == "" ]]; then
+    warn "3rd parameter must be either 'test' or empty, got: '$DRY_RUN'"
+    usage
+    exit 1
+  fi
+fi
+
+
 if [[ "$CODE_BASE" == "CE" ]]; then
-  echo "Adding to:      Kong open source"
+  msg "Adding to: Kong open source"
 elif [[ "$CODE_BASE" == "EE" ]]; then
-  echo "Adding to:      Kong Enterprise"
+  msg "Adding to: Kong Enterprise"
 else
-  echo "code-base must be either 'CE' or 'EE'"
+  warn "code-base, 1st parameter, must be either 'CE' or 'EE', got: '$CODE_BASE'"
   usage
   exit 1
 fi
 
 
 if [[ "$ADD_VERSION" == "" ]]; then
-  echo "version is missing"
+  warn "version, 2nd parameter, is missing"
   usage
   exit 1
 fi
-echo "Version to add: $ADD_VERSION"
+msg "Version to add: $ADD_VERSION"
 
 #TODO: here check we're in a Pongo git repo, and on 'master' branch
 
 if $(version_exists $ADD_VERSION); then
-  echo "Version '$ADD_VERSION' is already available"
+  err "Version '$ADD_VERSION' is already available"
   exit 1
 fi
 
 VERSIONS_FILE=${LOCAL_PATH}/assets/kong_${CODE_BASE}_versions.ver
 if [[ ! -f $VERSIONS_FILE ]]; then
-  echo "Versions file '$VERSIONS_FILE' not found"
+  err "Versions file '$VERSIONS_FILE' not found"
   exit 1
 fi
 
@@ -60,7 +72,7 @@ echo "$ADD_VERSION" >> $VERSIONS_FILE
 sort --version-sort $VERSIONS_FILE > ${VERSIONS_FILE}_tmp
 
 if [[ ! -f ${VERSIONS_FILE}_tmp ]]; then
-  echo "Failed to add and sort the new versions file"
+  err "Failed to add and sort the new versions file"
   exit 1
 fi
 
@@ -88,8 +100,8 @@ source ${LOCAL_PATH}/assets/update_versions.sh
 update_artifacts
 
 if [[ ! "$?" == "99" ]]; then
-  echo "Nothing was updated? nothing to commit. Please check the version"
-  echo "you have added to be a valid one."
+  warn "Nothing was updated? nothing to commit. Please check the version"
+  warn "you have added to be a valid one."
   git checkout $PREVIOUS_BRANCH
   git branch -D $BRANCH_NAME
   exit 1
@@ -104,13 +116,24 @@ else
 fi
 
 # push to remote anmd create a PR
-git push --set-upstream origin $BRANCH_NAME
-echo
-echo "Now creating a Github pull-request:"
-hub pull-request --no-edit
-echo
-echo "Success! A new branch '$BRANCH_NAME' was pushed to the repo (and a PR created)"
-echo "with the following commits:"
+if [[ "$DRY_RUN" == "" ]]; then
+  git push --set-upstream origin $BRANCH_NAME
+else
+  warn "[TEST-RUN skipping] git push --set-upstream origin $BRANCH_NAME"
+fi
+msg "Now creating a Github pull-request:"
+if [[ "$DRY_RUN" == "" ]]; then
+  hub pull-request --no-edit
+else
+  warn "[TEST-RUN skipping] hub pull-request --no-edit"
+fi
+
+msg "Success! A new branch '$BRANCH_NAME' was pushed to the repo (and a PR created)"
+msg "with the following commits:"
 git log --oneline -2
 echo
 git checkout $PREVIOUS_BRANCH &> /dev/null
+msg "done. Goto https://github.com/kong/kong-pongo/pulls to checkout the new PR."
+if [[ ! "$DRY_RUN" == "" ]]; then
+  warn "test-run completed, nothing was written! re-run without 'test' to push the changes."
+fi
