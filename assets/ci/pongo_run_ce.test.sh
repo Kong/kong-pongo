010e6e6910a36dab024bd0bae6cfaf5ebec288f2
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-09-01 16:09:19 +0200

    feat(ci) add version based testing setup

---
+++ assets/ci/pongo_run_ce.test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+function run_test {
+  # shellcheck disable=SC1090  # do not follow source
+  source "$(dirname "$(realpath "$0")")/pongo_run.helper.sh"
+
+  # testing Kong CE, with the 99 latest releases (not taking patch releases into account)
+  run_version_test "Kong" 99
+}
+
+# No need to modify anything below this comment
+
+# shellcheck disable=SC1090  # do not follow source
+[[ "$T_PROJECT_NAME" == "" ]] && set -e && source "${1:-$(dirname "$(realpath "$0")")/test.sh}" && set +e
+run_test
