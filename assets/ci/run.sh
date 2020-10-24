7ad2f51127c184edbf0862d57cfc1b63f8a2d60f
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-10-24 10:10:33 +0200

    fix(tests) allow to run individual testfiles again

--- assets/ci/run.sh
+++ assets/ci/run.sh
@@ -4,6 +4,15 @@
 # this `run.sh` script with an identical command line.
 # So instead of copying `test.sh` into your project, copy `run.sh`.
 
-# source: https://github.com/Tieske/run.sh
+# source repo: https://github.com/Tieske/test.sh
 
-curl https://raw.githubusercontent.com/Tieske/test.sh/master/test.sh | bash /dev/stdin "$@"
+
+
+if [[ $0 == "${BASH_SOURCE[0]}" ]]; then
+  # this script is executed, so test.sh must be executed
+  bash  "/dev/stdin" "$@" <<<"$( curl -sS https://raw.githubusercontent.com/Tieske/test.sh/master/test.sh )"
+else
+  # we're sourced, so have to source test.sh
+  # shellcheck disable=SC1091  # do not follow source
+  source  "/dev/stdin" "$@" <<<"$( curl -sS https://raw.githubusercontent.com/Tieske/test.sh/master/test.sh )"
+fi
