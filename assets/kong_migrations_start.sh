e1ba0bca36bfcd1435e3ea0768aae69512bda2e1
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-06-27 15:52:28 +0200

    refactor(shell) move complex aliases into scripts
    for easier maintenance

---
+++ assets/kong_migrations_start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# will (re)start Kong with a clean database, and an optional imported config
+
+kong stop
+kong migrations bootstrap --force
+
+echo ''
+unset KMS_FILENAME
+if [ -f /kong-plugin/kong.yaml ]; then
+    KMS_FILENAME=kong.yaml
+elif [ -f /kong-plugin/kong.yml ]; then
+    KMS_FILENAME=kong.yml
+elif [ -f /kong-plugin/kong.json ]; then
+    KMS_FILENAME=kong.json
+else
+    echo 'Declarative file "kong.yaml/yml/json" not found, skipping import'
+fi
+
+echo "Found \"$KMS_FILENAME\", importing declarative config..."
+kong config db_import /kong-plugin/$KMS_FILENAME
+if [ $? -ne 0 ]; then
+    echo "Failed to import \"$KMS_FILENAME\""
+    exit 1
+fi
+
+kong start
---
+++ assets/kong_start_dbless.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# will (re)start Kong DBless from a config file; kong.y(a)ml/json
+
+unset KDBL_FILENAME
+if [ -f /kong-plugin/kong.yaml ]; then
+    KDBL_FILENAME=kong.yaml
+elif [ -f /kong-plugin/kong.yml ]; then
+    KDBL_FILENAME=kong.yml
+elif [ -f /kong-plugin/kong.json ]; then
+    KDBL_FILENAME=kong.json
+else
+    echo 'Error: Declarative file "kong.yaml/yml/json" not found';
+    exit 1
+fi
+
+KONG_DATABASE=off KONG_DECLARATIVE_CONFIG=/kong-plugin/$KDBL_FILENAME kong restart
