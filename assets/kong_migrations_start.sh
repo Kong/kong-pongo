e2ff43d0a4859ef9180e941c123428886522bac8
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-06-29 14:03:26 +0200

    fix(alias) start Kong if no declarative file

--- assets/kong_migrations_start.sh
+++ assets/kong_migrations_start.sh
@@ -13,15 +13,18 @@ elif [ -f /kong-plugin/kong.yml ]; then
     KMS_FILENAME=kong.yml
 elif [ -f /kong-plugin/kong.json ]; then
     KMS_FILENAME=kong.json
-else
-    echo 'Declarative file "kong.yaml/yml/json" not found, skipping import'
 fi
+# fi
 
-echo "Found \"$KMS_FILENAME\", importing declarative config..."
-kong config db_import /kong-plugin/$KMS_FILENAME
-if [ $? -ne 0 ]; then
-    echo "Failed to import \"$KMS_FILENAME\""
-    exit 1
+if [ "$KMS_FILENAME" = "" ]; then
+    echo 'Declarative file "kong.yaml/yml/json" not found, skipping import'
+else
+    echo "Found \"$KMS_FILENAME\", importing declarative config..."
+    kong config db_import /kong-plugin/$KMS_FILENAME
+    if [ $? -ne 0 ]; then
+        echo "Failed to import \"$KMS_FILENAME\""
+        exit 1
+    fi
 fi
 
 kong start
