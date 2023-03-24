381d2eede9e797af0d2336447f1c10b8d01dbf01
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-03-24 13:24:10 +0100

    feat(shell): add alias to enable RBAC and GUI-auth

--- assets/pongo_profile.sh
+++ assets/pongo_profile.sh
@@ -9,6 +9,7 @@ alias kp='kong stop'
 alias kms='/pongo/kong_migrations_start.sh'
 alias kdbl='/pongo/kong_start_dbless.sh'
 alias kx='/pongo/kong_export.sh'
+alias kauth='. /pongo/kong_setup_auth.sh'
 
 # We want this to output without expanding variables
 # shellcheck disable=SC2016
@@ -16,10 +17,11 @@ echo 'PS1="\[\e[00m\]\[\033[1;34m\][$PS1_KONG_VERSION:\[\e[91m\]$PS1_REPO_NAME\$
 
 echo ""
 echo "Get started quickly with the following aliases/shortcuts:"
-echo "  kms  - kong migrations start; wipe/initialize the database and start Kong clean,"
-echo "         importing declarative configuration if available."
-echo "  kdbl - kong start dbless; start Kong in dbless mode, requires a declarative configuration."
-echo "  ks   - kong start; starts Kong with the existing database contents (actually a restart)."
-echo "  kp   - kong stop; stop Kong."
-echo "  kx   - export the current Kong database to a declarative configuration file."
+echo "  kms   - kong migrations start; wipe/initialize the database and start Kong clean,"
+echo "          importing declarative configuration if available."
+echo "  kdbl  - kong start dbless; start Kong in dbless mode, requires a declarative configuration."
+echo "  ks    - kong start; starts Kong with the existing database contents (actually a restart)."
+echo "  kp    - kong stop; stop Kong."
+echo "  kx    - export the current Kong database to a declarative configuration file."
+echo "  kauth - setup authentication (RBAC and GUI-auth)."
 echo ""
