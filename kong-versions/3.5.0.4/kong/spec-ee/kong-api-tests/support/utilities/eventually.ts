93562e23df87aed4c50f7af86df360305bfa62ed
Zachary Hu <zachary.hu@konghq.com>
2024-06-18 20:59:19 +0800

    chore(version) added Kong Enterprise version 3.5.0.4 artifacts

---
+++ kong-versions/3.5.0.4/kong/spec-ee/kong-api-tests/support/utilities/eventually.ts
@@ -0,0 +1,36 @@
+/**
+ * Wait for the `assertions` does not throw any exceptions.
+ * @param assertions - The assertions to be executed.
+ * @param timeout - The timeout in milliseconds.
+ * @param interval - The interval in milliseconds.
+ * @param verbose - Verbose logs in case of error
+ * @returns {Promise<void>} - Asnyc void promise.
+ */
+export const eventually = async (
+  assertions: () => Promise<void>,
+  timeout = 15000,
+  interval = 3000,
+  verbose = false,
+): Promise<void> => {
+  let errorMsg = '';
+
+  while (timeout >= 0) {
+    const start = Date.now();
+    try {
+      await assertions();
+      return;
+    } catch (error: any) {
+      const end = Date.now();
+      if (verbose) {
+        errorMsg = error.message;
+        console.log(errorMsg);
+        console.log(
+            `** Assertion(s) Failed -- Retrying in ${interval / 1000} seconds **`
+        );
+      }
+      await new Promise((resolve) => setTimeout(resolve, interval));
+      timeout -= interval + (end - start);
+    }
+  }
+  throw new Error(errorMsg);
+};
