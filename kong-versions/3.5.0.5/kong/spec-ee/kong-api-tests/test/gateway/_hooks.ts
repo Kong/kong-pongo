68a135836e04c35501db243791e50a5ca8296a13
Zachary Hu <zachary.hu@konghq.com>
2024-06-21 16:22:30 +0800

    chore(version) added Kong Enterprise version 3.5.0.5 artifacts

---
+++ kong-versions/3.5.0.5/kong/spec-ee/kong-api-tests/test/gateway/_hooks.ts
@@ -0,0 +1,37 @@
+import { clearAllKongResources, createRedisClient, gatewayAuthHeader, isCI, waitForConfigRebuild } from '@support';
+import {
+  postGatewayEeLicense,
+  deleteGatewayEeLicense,
+} from '@shared/gateway_workflows';
+import axios from 'axios';
+
+export const mochaHooks: Mocha.RootHookObject = {
+  beforeAll: async function (this: Mocha.Context) {
+    try {
+        // Set Auth header for Gateway Admin requests
+        const { authHeaderKey, authHeaderValue } = gatewayAuthHeader();
+        axios.defaults.headers[authHeaderKey] = authHeaderValue;
+        createRedisClient();
+      if (isCI()) {
+        // Gateway for API tests starts without EE_LICENSE in CI, hence, we post license at the beginning of all tests to allow us test the functionality of license endpoint
+        await postGatewayEeLicense();
+        // Wait for the license propagation to complete before release to the test
+        await waitForConfigRebuild();
+        console.info('waitForConfigRebuild successfully executed after posting the ee license\n')
+      }
+      createRedisClient();
+    } catch (err) {
+      console.error(`Something went wrong in beforeAll hook while rebuilding configuration: ${err}\n`)
+
+      // remove all possible remnant entities from failed waitForConfigRebuild above to start tests from clean state and avoid flakiness
+      await clearAllKongResources();
+    }
+  },
+
+  afterAll: async function (this: Mocha.Context) {
+    // Gateway for API tests starts without EE_LICENSE in CI, hence, we delete license at the end of all tests to allow test rerun from clean state
+    if (isCI()) {
+      await deleteGatewayEeLicense();
+    }
+  },
+};
