d85d951bfc0f83297ac61f4209537c3297ca259f
Zachary Hu <zachary.hu@konghq.com>
2024-06-22 13:06:26 +0800

    chore(version) added Kong Enterprise version 2.8.4.10 artifacts

---
+++ kong-versions/2.8.4.10/kong/spec-ee/fixtures/mock_distributions_constants.lua
@@ -0,0 +1,53 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
+local c = {}
+
+c.plugins = {
+  "kafka-upstream",
+}
+
+c.featureset = {
+  full = {
+    conf = {},
+  },
+  full_expired = {
+    conf = {},
+    allow_admin_api = {
+      ["/licenses"] = { ["*"] = true },
+      ["/licenses/:licenses"] = { ["*"] = true },
+    },
+  },
+  free = {
+    conf = {
+      enforce_rbac = "off",
+      -- XXX need to keep this alias to enforce_rbac
+      rbac = "off",
+      vitals = false,
+      anonymous_reports = true,
+      portal = false,
+      event_hooks_enabled = false,
+      -- NOOP (unset it)
+      admin_gui_auth = function() end,
+    },
+    allow_admin_api = {
+      -- Allow these granularly
+      ["/workspaces"] = { GET = true, OPTIONS = true },
+      ["/workspaces/:workspaces"] = { GET = true, OPTIONS = true },
+    },
+    deny_admin_api = {
+      -- Deny any other
+      ["/workspaces"] = { ["*"] = true },
+      ["/workspaces/:workspaces"] = { ["*"] = true },
+    },
+  }
+}
+
+-- This is a flag is being used to indicate a generated release
+c.release = false
+
+return setmetatable(c, {__index = function() return {} end })
