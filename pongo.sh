6ce752cc3b4ba1eac37848d2063a182fc4690c71
Water-Melon <melon-c@hotmail.com>
2024-09-19 11:19:17 +0000

    remove env KONG_TEST_LICENSE_PATH

--- pongo.sh
+++ pongo.sh
@@ -1182,7 +1182,6 @@ function main {
       -e KONG_LICENSE_DATA \
       -e KONG_TEST_DONT_CLEAN \
       -e KONG_TEST_FIPS \
-      -e KONG_TEST_LICENSE_PATH \
       -e http_proxy \
       -e https_proxy \
       -e no_proxy \
