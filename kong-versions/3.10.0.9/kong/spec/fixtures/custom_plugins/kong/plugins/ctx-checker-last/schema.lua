d0abe7b1b690148d1e220df9d41ba29ff5f7f9b2
ms2008 <ms2008vip@gmail.com>
2026-03-20 10:28:49 +0800

    chore(version) added Kong Enterprise version 3.10.0.9 artifacts

---
+++ kong-versions/3.10.0.9/kong/spec/fixtures/custom_plugins/kong/plugins/ctx-checker-last/schema.lua
@@ -0,0 +1,27 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
+return {
+  name = "ctx-checker-last",
+  fields = {
+    {
+      config = {
+        type = "record",
+        fields = {
+          { ctx_set_field   = { type = "string" } },
+          { ctx_set_value   = { type = "string", default = "set_by_ctx_checker" } },
+          { ctx_set_array   = { type = "array", elements = { type = "string" } } },
+          { ctx_check_field = { type = "string" } },
+          { ctx_check_value = { type = "string" } },
+          { ctx_check_array = { type = "array", elements = { type = "string" } } },
+          { ctx_kind        = { type = "string", default = "ngx.ctx" } },
+          { ctx_throw_error = { type = "boolean", default = false } },
+        },
+      },
+    },
+  },
+}
